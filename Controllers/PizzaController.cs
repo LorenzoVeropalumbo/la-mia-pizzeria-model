cf7ee7ffc1d41c2db0fff84f8bd91cc8b0e25778
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-11-21 15:19:24 +0100

    RIedito il layout

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,30 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            PizzaDbContext db = new PizzaDbContext();
+
+            List<Pizza> listOfPizzas = db.Pizzas.ToList();
+
+            return View(listOfPizzas);
+        }
+        public IActionResult Detail(int id)
+        {
+
+            PizzaDbContext db = new PizzaDbContext();
+
+            Pizza pizza = db.Pizzas.Where(p => p.Id == id).FirstOrDefault();
+
+            return View(pizza);
+        }
+    }
+
+
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public float Price { get; set; }
+
+        public Pizza()
+        {
+
+        }
+
+        public Pizza(int id, string title, string description, string image, float price)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+    }
+}
